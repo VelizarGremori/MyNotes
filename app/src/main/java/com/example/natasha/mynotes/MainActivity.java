158256327df319cd0f3c30c5eae70c6735716c2a
Natasha <velizargremori@mail.ru>
2018-08-15 22:20:16 +0500

    Commit

---
+++ app/src/main/java/com/example/natasha/mynotes/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.natasha.mynotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tablelayout);
+
+
+    }
+}
