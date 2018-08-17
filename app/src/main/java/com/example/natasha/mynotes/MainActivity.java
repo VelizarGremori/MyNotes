eb0ae3515302e8607c7dd87ac8caf5c7ebe1c44e
Кирилл <kt@lime-it.ru>
2018-08-17 16:48:08 +0500

    Поправил версию SDK Реализовал добавление строк в таблицу

--- app/src/main/java/com/example/natasha/mynotes/MainActivity.java
+++ app/src/main/java/com/example/natasha/mynotes/MainActivity.java
@@ -1,18 +1,72 @@
 package com.example.natasha.mynotes;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.*;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.tablelayout);
+        context=this;
 
 
+        final TableLayout tableLayout = (TableLayout)findViewById(R.id.tablelayout);
+
+        Button button = (Button)findViewById(R.id.but);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TableRow tableRow = crateTableRow();
+
+                tableLayout.addView(tableRow);
+
+            }
+        });
+    }
+
+    public TableRow crateTableRow(){
+
+        TableRow tableRow = new TableRow(context);
+
+        tableRow.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT));
+
+        TextView data = new TextView(context);
+        data.setText("17.08.2018");
+
+        EditText editTextName = (EditText) findViewById(R.id.name);
+        TextView name = new TextView(context);
+        name.setText(editTextName.getText());
+
+        EditText editTextNum = (EditText) findViewById(R.id.num);
+        TextView num = new TextView(context);
+        num.setText(editTextNum.getText());
+
+        EditText editTextSum = (EditText) findViewById(R.id.sum);
+        TextView sum = new TextView(context);
+        sum.setText(editTextSum.getText());
+
+        tableRow.addView(data);
+        tableRow.addView(name);
+        tableRow.addView(num);
+        tableRow.addView(sum);
+
+        return tableRow;
     }
 }
